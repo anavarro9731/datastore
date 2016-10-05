1b9b21eb8260aa456328c64adef2f2f170f779d1
Aaron Navarro <anavarro@neworbit.co.uk>
2016-10-05 21:38:47 +0100

    syntax

--- src/DataStore.Impl.DocumentDb/DocumentRepository.cs
+++ src/DataStore.Impl.DocumentDb/DocumentRepository.cs
@@ -13,15 +13,17 @@
 
     public class DocumentRepository : IDocumentRepository
     {
-        private readonly DocumentDbSettings config;
+        private readonly DocumentDbSettings _config;
 
-        private readonly DocumentClient documentClient;
+        private readonly DocumentClient _documentClient;
 
         public DocumentRepository(DocumentDbSettings config)
         {
-            documentClient = new DocumentDbClientFactory(config).GetDocumentClient();
-            this.config = config;
+            _documentClient = new DocumentDbClientFactory(config).GetDocumentClient();
+            this._config = config;
         }
+
+        #region IDocumentRepository Members
 
         public async Task<T> AddAsync<T>(AggregateAdded<T> aggregateAdded) where T : IHaveAUniqueId
         {
@@ -34,39 +36,39 @@
 
             var result =
                 await
-                DocumentDbUtils.ExecuteWithRetries(
-                    () =>
-                    documentClient.CreateDocumentAsync(
-                        config.DatabaseSelfLink(),
-                        aggregateAdded.Model,
-                        disableAutomaticIdGeneration: disableAutoIdGeneration));
+                    DocumentDbUtils.ExecuteWithRetries(
+                        () =>
+                            _documentClient.CreateDocumentAsync(
+                                _config.DatabaseSelfLink(),
+                                aggregateAdded.Model,
+                                disableAutomaticIdGeneration: disableAutoIdGeneration));
 
-            return (T)(dynamic)result.Resource;
+            return (T) (dynamic) result.Resource;
         }
 
         public IQueryable<T> CreateDocumentQuery<T>() where T : IHaveAUniqueId, IHaveSchema
         {
             var name = typeof(T).Name;
-            var query = documentClient.CreateDocumentQuery<T>(config.DatabaseSelfLink()).Where(item => item.Schema == name);
+            var query = _documentClient.CreateDocumentQuery<T>(_config.DatabaseSelfLink()).Where(item => item.Schema == name);
             return query;
         }
 
-        public async Task<T> DeleteHardAsync<T>(AggregateHardDeleted<T> aggregateHardDeleted) where T: IHaveAUniqueId
+        public async Task<T> DeleteHardAsync<T>(AggregateHardDeleted<T> aggregateHardDeleted) where T : IHaveAUniqueId
         {
             var docLink = CreateDocumentSelfLinkFromId(aggregateHardDeleted.Model.id);
-            return (T)(dynamic)(await DocumentDbUtils.ExecuteWithRetries(() => documentClient.DeleteDocumentAsync(docLink))).Resource;
+            return (T) (dynamic) (await DocumentDbUtils.ExecuteWithRetries(() => _documentClient.DeleteDocumentAsync(docLink))).Resource;
         }
 
-        public async Task<T> DeleteSoftAsync<T>(AggregateSoftDeleted<T> aggregateSoftDeleted) where T: IHaveAUniqueId
+        public async Task<T> DeleteSoftAsync<T>(AggregateSoftDeleted<T> aggregateSoftDeleted) where T : IHaveAUniqueId
         {
             var document = await GetItemAsync(aggregateSoftDeleted.Model.id);
             document.SetPropertyValue(nameof(IAggregate.Active), false);
-            return (T)(dynamic)(await DocumentDbUtils.ExecuteWithRetries(() => documentClient.ReplaceDocumentAsync(document.SelfLink, document))).Resource;
+            return (T) (dynamic) (await DocumentDbUtils.ExecuteWithRetries(() => _documentClient.ReplaceDocumentAsync(document.SelfLink, document))).Resource;
         }
 
         public void Dispose()
         {
-            documentClient.Dispose();
+            _documentClient.Dispose();
         }
 
         public async Task<IEnumerable<T>> ExecuteQuery<T>(IQueryable<T> query) where T : IHaveAUniqueId
@@ -85,7 +87,7 @@
 
         public async Task<bool> Exists(Guid id)
         {
-            var query = this.documentClient.CreateDocumentQuery(this.config.DatabaseSelfLink()).Where(item => item.Id == id.ToString()).AsDocumentQuery();
+            var query = _documentClient.CreateDocumentQuery(_config.DatabaseSelfLink()).Where(item => item.Id == id.ToString()).AsDocumentQuery();
             var results = await query.ExecuteNextAsync();
             return results.Count > 0;
         }
@@ -93,14 +95,14 @@
         public async Task<T> GetItemAsync<T>(Guid id) where T : IHaveAUniqueId
         {
             var result = await GetItemAsync(id);
-            return (T)(dynamic)result;
+            return (T) (dynamic) result;
         }
 
         public async Task<Document> GetItemAsync(Guid id)
         {
             try
             {
-                var result = await documentClient.ReadDocumentAsync(CreateDocumentSelfLinkFromId(id));
+                var result = await _documentClient.ReadDocumentAsync(CreateDocumentSelfLinkFromId(id));
                 if (result == null)
                 {
                     throw new DatabaseRecordNotFoundException(id.ToString());
@@ -117,13 +119,15 @@
         public async Task<T> UpdateAsync<T>(AggregateUpdated<T> aggregateUpdated) where T : IHaveAUniqueId
         {
             return
-                (T)(dynamic)(await
-                 DocumentDbUtils.ExecuteWithRetries(
-                     () =>
-                     documentClient.ReplaceDocumentAsync( 
-                         CreateDocumentSelfLinkFromId(aggregateUpdated.Model.id),
-                         aggregateUpdated.Model))).Resource;
+                (T) (dynamic) (await
+                    DocumentDbUtils.ExecuteWithRetries(
+                        () =>
+                            _documentClient.ReplaceDocumentAsync(
+                                CreateDocumentSelfLinkFromId(aggregateUpdated.Model.id),
+                                aggregateUpdated.Model))).Resource;
         }
+
+        #endregion
 
         private Uri CreateDocumentSelfLinkFromId(Guid id)
         {
@@ -132,7 +136,7 @@
                 throw new ArgumentException("Id is required for update/delete/read operation");
             }
 
-            var docLink = UriFactory.CreateDocumentUri(config.DatabaseName, config.DefaultCollectionName, id.ToString());
+            var docLink = UriFactory.CreateDocumentUri(_config.DatabaseName, _config.DefaultCollectionName, id.ToString());
             return docLink;
         }
     }
