a3f867d28d69d2cf39fffbe6fde81521964ca6d7
Aaron Navarro <anavarro@neworbit.co.uk>
2016-10-08 17:04:08 +0100

    add batch to create nuget packages

---
+++ src/pack.cmd
@@ -0,0 +1,42 @@
+cd DataStore
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Infrastructure
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Interfaces
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Messages
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Impl.DocumentDb
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Addons
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
+cd DataStore.Models
+del bin\RELEASE\*.nupkg
+dotnet pack -c RELEASE --version-suffix %1
+copy bin\RELEASE\*.nupkg c:\Nuget.Local
+
+cd ..
