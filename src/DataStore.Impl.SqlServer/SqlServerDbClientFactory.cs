86ae2d9d4dc963fb3e96e5fac2254a0985a58987
Aaron Navarro <anavarro9731@gmail.com>
2017-04-06 23:09:44 +0100

    working sql impl

---
+++ src/DataStore.Impl.SqlServer/SqlServerDbClientFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace DataStore.Impl.SqlServer
+{
+    public class SqlServerDbClientFactory
+    {
+        private readonly SqlServerDbSettings config;
+
+        public SqlServerDbClientFactory(SqlServerDbSettings config)
+        {
+            this.config = config;            
+        }
+            
+        public SqlConnection GetClient()
+        {
+            return new SqlConnection(config.ConnectionString);
+        }
+    }
+
+    
+}
---
+++ src/DataStore.Impl.SqlServer/SqlServerDbInitialiser.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace DataStore.Impl.SqlServer
+{
+    public class SqlServerDbInitialiser
+    {
+        public static void Initialise(SqlServerDbClientFactory factory)
+        {
+            {
+                TryCreateTable();
+            }
+
+            void TryCreateTable()
+            {
+                using (var connection = factory.GetClient())
+                {
+                    connection.Open();
+                    try
+                    {
+                        using (var command = new SqlCommand(
+                            "CREATE TABLE DataStoreAggregates (AggregateId uniqueidentifier, Schema nvarchar(250), Json nvarchar(max))", connection))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    catch
+                    {
+                        // ignored
+                        // wiil not work if table already exists
+                    }
+                }
+            }
+        }
+    }
+}
