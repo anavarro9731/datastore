8b16ccd230b45c2098ab6519a7c283cca37be2c1
Aaron Navarro <anavarro9731@gmail.com>
2017-04-17 20:26:46 +0100

    add deleteallcollections for use after azure test runs to save dosh

---
+++ src/DataStore.Tests/Tests/AzureOnly/DeleteAllCollections.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStore.Tests.Tests.AzureOnly
+{
+    using TestHarness;
+    using Xunit;
+
+    public class DeleteAllCollections
+    {
+        
+        [Fact(Skip = "Used only for maintenance")]
+        //[Fact]
+        public void CanDeleteAllCollections()
+        {
+            var harness = TestHarnessFunctions.GetDocumentDbTestHarness(nameof(CanDeleteAllCollections)) as DocumentDbTestHarness;
+            harness.RemoveAllCollections();
+        }
+    }
+}
