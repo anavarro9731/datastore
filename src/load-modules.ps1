8e3e4f5dcee762157046320520ed209ebef285f5
anavarro <anavarro9731@gmail.com>
2017-10-03 12:48:59 +0100

    remove creds

--- src/load-modules.ps1
+++ src/load-modules.ps1
@@ -1,33 +1,44 @@
-$vstsCredentials = @("anavarro9731","VST`"04`"supremus")
-$vstsRootUri = "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath="
-$modules = @(
- "build",
- "prepare-new-version",
- "build-and-test",
- "pack-and-publish"
-)
+function global:LoadModules {
 
-# Set Project Root Folder
-$global:projectRoot = $PSScriptRoot
-Push-Location $PSScriptRoot
+	Param(
+        [Alias('u')]
+		[string] $vstsUser,
+        [Alias('p')]
+        [string] $vstsPassword
+	)
 
-Write-Host "Importing Custom Modules"
+    $vstsCredentials = @("anavarro9731","VST`"04`"supremus")
+    $vstsRootUri = "https://anavarro9731.visualstudio.com/defaultcollection/powershell/_apis/git/repositories/powershell/items?api-version=1.0&scopepath="
+    $modules = @(
+     "build",
+     "prepare-new-version",
+     "build-and-test",
+     "pack-and-publish"
+    )
 
-# Base64-encodes the Personal Access Token (PAT) appropriately
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $vstsCredentials[0],$vstsCredentials[1])))
+    # Set Project Root Folder
+    $global:projectRoot = $PSScriptRoot
+    Push-Location $PSScriptRoot
 
-# Set Modules Path for Session
-$env:PSModulePath = $env:PSModulePath + ";$PSScriptRoot\.psModules\"
+    Write-Host "Importing Custom Modules"
 
-# Download and Import all Modules
-foreach ($module in $modules) {
- New-Item ".\.psModules\$module\" -ItemType Directory -Verbose
- Invoke-RestMethod -Uri "$vstsRootUri$module.psm1" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "text/plain; charset=UTF-8" -OutFile ".\.psModules\$module\$module.psm1"
- Import-Module $module -Verbose -Global -Force
+    # Base64-encodes the Personal Access Token (PAT) appropriately
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $vstsCredentials[0],$vstsCredentials[1])))
+
+    # Set Modules Path for Session
+    $env:PSModulePath = $env:PSModulePath + ";$PSScriptRoot\.psModules\"
+
+    # Download and Import all Modules
+    foreach ($module in $modules) {
+     New-Item ".\.psModules\$module\" -ItemType Directory -Verbose
+     Invoke-RestMethod -Uri "$vstsRootUri$module.psm1" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -ContentType "text/plain; charset=UTF-8" -OutFile ".\.psModules\$module\$module.psm1"
+     Import-Module $module -Verbose -Global -Force
+    }
+
+    #Remove folder once modules are loaded
+    Remove-Item ".\.psModules\" -Verbose -Recurse
+
 }
-
-#Remove folder once modules are loaded
-Remove-Item ".\.psModules\" -Verbose -Recurse
 
 #expose this function like a CmdLet
 function global:Run {
@@ -36,7 +47,9 @@ function global:Run {
 	Param(
 		[switch]$PrepareNewVersion,
 		[switch]$BuildAndTest,
-		[switch]$PackAndPublish
+		[switch]$PackAndPublish,
+        [Alias('o')]
+        [string] $originUrl
 	)
 	
 	if ($PrepareNewVersion) {
@@ -72,6 +85,7 @@ function global:Run {
         ) `
         -mygetFeedUri "https://www.myget.org/F/anavarro9731/api/v2/package" `
         -mygetSymbolFeedUri "https://www.myget.org/F/anavarro9731/symbols/api/v2/package" `
-        -mygetApiKey "7cde1967-fe13-4672-91ef-f1deb3543e78" 
+        -mygetApiKey "7cde1967-fe13-4672-91ef-f1deb3543e78" `
+        -originUrl $originUrl
     }
 }
