50a8af847155db0cd25c2ecdf831e1cc8b782685
Aaron Navarro <anavarro9731@gmail.com>
2019-03-08 16:10:57 +0000

    fix compiler warning

--- src/DataStore.Interfaces.LowLevel/IEntity.cs
+++ src/DataStore.Interfaces.LowLevel/IEntity.cs
@@ -2,7 +2,5 @@ namespace DataStore.Interfaces.LowLevel
 {
     public interface IEntity : IHaveAUniqueId, IRememberWhenIWasCreated, IHaveSchema
     {
-        //required lowercase when a docdb partitionkey
-        string schema { get; set; }
     }
 }
