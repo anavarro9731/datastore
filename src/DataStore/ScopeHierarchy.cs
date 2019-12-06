c263ff7dbf5c7989a9642a0d939c128032cb2431
Aaron Navarro <anavarro9731@gmail.com>
2019-12-06 14:03:50 +0000

    add method to check for a match against an expanded permissionscope refactor public api of some objects

--- src/DataStore/ScopeHierarchy.cs
+++ src/DataStore/ScopeHierarchy.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Linq.Expressions;
     using System.Threading.Tasks;
     using CircuitBoard.Permissions;
     using global::DataStore.Interfaces;
@@ -26,38 +25,28 @@
 
         private interface IScopeLevel
         {
-            Task<List<EntityWithChildren>> HydrateScopeLevel(IDataStore dataStore);
-
             string EntityTypeName { get; }
+
+            Task<List<EntityWithChildren>> HydrateScopeLevel(IDataStore dataStore);
         }
 
-        
-        public async Task<IEnumerable<IHaveScope>> ExtrapolatedIntersection(List<IHaveScope> dataWithScope, List<ScopeReference> userPermissionScopes, IDataStore dataStore)
+        public async Task<bool> ExpandedPermissionContains(IPermissionInstance permissionInstance, IDataStore dataStore, ScopeReference scopeToMatch)
         {
-            {
-                if (this.scopeEntityLookup.Count == 0) await HydrateHierarchy(dataStore).ConfigureAwait(false); ;
+            if (this.scopeEntityLookup.Count == 0) await HydrateHierarchy(dataStore).ConfigureAwait(false);
+            ;
 
-                var extrapolatedScopes = new List<ScopeReference>(userPermissionScopes);
-
-                foreach (var userPermissionScope in userPermissionScopes)
-                    if (this.scopeEntityLookup.ContainsKey(userPermissionScope.ScopeObjectId))
-                    {
-                        var currentScopeReferencedEntity = this.scopeEntityLookup[userPermissionScope.ScopeObjectId];
-                        RecurseAndFindNewScopeReferences(currentScopeReferencedEntity, ref extrapolatedScopes);
-                    }
-
-                return dataWithScope.Where(sd => sd.ScopeReferences.Intersect(extrapolatedScopes).Any());
-            }
-
-            void RecurseAndFindNewScopeReferences(EntityWithChildren referencedEntity, ref List<ScopeReference> extrapolatedScopesBuffer)
-            {
-                if (referencedEntity.Children.Any())
+            var extrapolatedScopes = new List<ScopeReference>();
+            foreach (var userPermissionScope in permissionInstance.ScopeReferences)
+                if (this.scopeEntityLookup.ContainsKey(userPermissionScope.ScopeObjectId))
                 {
-                    extrapolatedScopesBuffer.AddRange(referencedEntity.Children.Select(c => new ScopeReference(c.id, c.EntityTypeName, scopeObjectDebugId: c.ScopeReferenceId)));
-                    foreach (var referencedEntityChild in referencedEntity.Children)
-                        RecurseAndFindNewScopeReferences(referencedEntityChild, ref extrapolatedScopesBuffer);
+                    var currentScopeReferencedEntity = this.scopeEntityLookup[userPermissionScope.ScopeObjectId];
+                    RecurseAndFindNewScopeReferences(currentScopeReferencedEntity, ref extrapolatedScopes);
                 }
-            }
+
+            var result = permissionInstance.ScopeReferences.Contains(scopeToMatch) /*better check the required scope is not already present*/
+                         || extrapolatedScopes.Contains(scopeToMatch);
+
+            return result;
         }
 
         public ScopeHierarchy WithScopeLevel<T>(Func<T, Guid> parentIdSelector) where T : class, IAggregate, new()
@@ -71,15 +60,36 @@
             return this;
         }
 
+        internal async Task<IEnumerable<IHaveScope>> Expanded(List<IHaveScope> dataWithScope, List<ScopeReference> userPermissionScopes, IDataStore dataStore)
+        {
+            {
+                if (this.scopeEntityLookup.Count == 0) await HydrateHierarchy(dataStore).ConfigureAwait(false);
+                ;
+
+                var extrapolatedScopes = new List<ScopeReference>(userPermissionScopes);
+
+                foreach (var userPermissionScope in userPermissionScopes)
+                    if (this.scopeEntityLookup.ContainsKey(userPermissionScope.ScopeObjectId))
+                    {
+                        var currentScopeReferencedEntity = this.scopeEntityLookup[userPermissionScope.ScopeObjectId];
+                        RecurseAndFindNewScopeReferences(currentScopeReferencedEntity, ref extrapolatedScopes);
+                    }
+
+                return dataWithScope.Where(sd => sd.ScopeReferences.Intersect(extrapolatedScopes).Any());
+            }
+        }
+
         private async Task HydrateHierarchy(IDataStore dataStore)
         {
             foreach (var scopeLevel in this.scopeLevels)
             {
-                var scopeEntities = await scopeLevel.HydrateScopeLevel(dataStore).ConfigureAwait(false); ;
+                var scopeEntities = await scopeLevel.HydrateScopeLevel(dataStore).ConfigureAwait(false);
+                ;
 
                 if (TheUserIsAddingAScopeLevelWhoseEntitiesCantBeTracedToAParent())
                 {
-                    throw new Exception($"Some of the entities in scope-level {scopeLevel.EntityTypeName} do not have links to a parent. This is only allowed for the first level.");
+                    throw new Exception(
+                        $"Some of the entities in scope-level {scopeLevel.EntityTypeName} do not have links to a parent. This is only allowed for the first level.");
                 }
 
                 foreach (var entity in scopeEntities)
@@ -102,7 +112,16 @@
                     return this.scopeEntityLookup.Count > 0 && AllEntitiesHaveParents() == false;
                 }
             }
+        }
 
+        private void RecurseAndFindNewScopeReferences(EntityWithChildren referencedEntity, ref List<ScopeReference> extrapolatedScopesBuffer)
+        {
+            if (referencedEntity.Children.Any())
+            {
+                extrapolatedScopesBuffer.AddRange(referencedEntity.Children.Select(c => new ScopeReference(c.id, c.EntityTypeName, c.ScopeReferenceId)));
+                foreach (var referencedEntityChild in referencedEntity.Children)
+                    RecurseAndFindNewScopeReferences(referencedEntityChild, ref extrapolatedScopesBuffer);
+            }
         }
 
         private class EntityWithChildren
@@ -116,6 +135,12 @@
                 this.aggregateImplementation = aggregateImplementation;
                 this.ParentId = parentId;
             }
+
+            public List<EntityWithChildren> Children { get; } = new List<EntityWithChildren>();
+
+            public string EntityTypeName => this.aggregateImplementation.GetType().FullName;
+
+            public Guid id { get => this.aggregateImplementation.id; set => this.aggregateImplementation.id = value; }
 
             public string ScopeReferenceId
             {
@@ -131,29 +156,20 @@
                     }
                 }
             }
-
-            public List<EntityWithChildren> Children { get; } = new List<EntityWithChildren>();
-
-            public string EntityTypeName => this.aggregateImplementation.GetType().FullName;
-
-            public Guid id { get => this.aggregateImplementation.id; set => this.aggregateImplementation.id = value; }
         }
 
         private class ScopeLevel<T> : IScopeLevel where T : class, IAggregate, new()
         {
             public Func<T, Guid> ParentIdSelector;
 
+            public string EntityTypeName => typeof(T).FullName;
+
             public async Task<List<EntityWithChildren>> HydrateScopeLevel(IDataStore dataStore)
             {
                 var aggregates = await dataStore.Read<T>().ConfigureAwait(false);
-                var projection = aggregates.Select(x =>
-                    {
-                        return new EntityWithChildren(x, this.ParentIdSelector(x));
-                    }).ToList();
+                var projection = aggregates.Select(x => { return new EntityWithChildren(x, this.ParentIdSelector(x)); }).ToList();
                 return projection;
             }
-
-            public string EntityTypeName => typeof(T).FullName;
         }
     }
 }
