52cede85fecebf581b6e0fe6d7df5b7846af96cd
Aaron Navarro <anavarro9731@gmail.com>
2019-12-13 15:31:36 +0000

    refactor to support underlying interface changes

--- src/DataStore.Tests/Tests/IDocumentRepository/Query/WhenCallingReadWithFiltration.cs
+++ src/DataStore.Tests/Tests/IDocumentRepository/Query/WhenCallingReadWithFiltration.cs
@@ -108,8 +108,8 @@ namespace DataStore.Tests.Tests.IDocumentRepository.Query
                     new ScopeReference(pi2s1.id, pi2s1.GetType().FullName, scopeObjectDebugId:pi2s1.Name)
                 });
 
-            user.PermissionInstances.Add(permission1Instance);
-            user.PermissionInstances.Add(permission2Instance);
+            user.AddPermission(permission1Instance);
+            user.AddPermission(permission2Instance);
 
             this.volvo1Id = Guid.NewGuid();
             var volvo1 = new Car
