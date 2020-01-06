1b584e5e8cf7c8d79853423617f9c641fd983ef9
anavarro <anavarro9731@gmail.com>
2020-01-06 15:52:21 +0000

    add console writer add query details to traces

---
+++ src/DataStore.Providers.CosmosDb/ConsoleWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStore.Providers.CosmosDb
+{
+    using System.Diagnostics;
+    using Xunit.Abstractions;
+
+    public class ConsoleWriter : ITestOutputHelper
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            Console.WriteLine(String.Format(format, args));
+        }
+    }
+}
