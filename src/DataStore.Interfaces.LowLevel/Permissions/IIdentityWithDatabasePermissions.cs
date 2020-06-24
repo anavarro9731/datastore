385fec62181b615fd573042a3b68be2fdf38b0c9
aaron navarro <anavarro9731@gmail.com>
2020-06-24 20:54:39 +1000

    refactor iidentitywithdatabasepermissions interface

--- src/DataStore.Interfaces.LowLevel/Permissions/IIdentityWithDatabasePermissions.cs
+++ src/DataStore.Interfaces.LowLevel/Permissions/IIdentityWithDatabasePermissions.cs
@@ -4,13 +4,8 @@
     using System.Collections.Generic;
     using CircuitBoard.Security;
 
-    public interface IIdentityWithDatabasePermissions
+    public interface IIdentityWithDatabasePermissions : IHaveAUniqueId
     {
-        // ReSharper disable once InconsistentNaming
-        Guid id { get; set; } //lowercase to support entities persisted in cosmosdb, grr!
-
-        string UserName { get; set; }
-
         bool HasPermission(IPermission permission);
 
         void AddPermission(IDatabasePermissionInstance permissionInstance);
