531f38f9f372d90059028b26e78715b3b2750738
Aaron Navarro <anavarro9731@gmail.com>
2025-07-01 20:57:35 +0100

    allow namespace changes

--- src/DataStore.Providers.CosmosDb/CosmosJsonDotNetSerializer.cs
+++ src/DataStore.Providers.CosmosDb/CosmosJsonDotNetSerializer.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using DataStore.Interfaces.LowLevel;
     using Microsoft.Azure.Cosmos;
@@ -14,14 +15,15 @@
 
         private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
         {
-            TypeNameHandling = TypeNameHandling.Auto,
-            ContractResolver = new CosmosMetadataPreservingResolver()
+            TypeNameHandling = TypeNameHandling.Auto, ContractResolver = new CosmosMetadataPreservingResolver(), SerializationBinder = new TypeNameOnlyBinder()
         };
 
         public override T FromStream<T>(Stream stream)
         {
             if (typeof(Stream).IsAssignableFrom(typeof(T)))
+            {
                 return (T)(object)stream;
+            }
 
             using (var sr = new StreamReader(stream))
             using (var jr = new JsonTextReader(sr))
@@ -35,7 +37,10 @@
         {
             var ms = new MemoryStream();
             var sw = new StreamWriter(ms, DefaultEncoding);
-            var jw = new JsonTextWriter(sw) { Formatting = Formatting.None };
+            var jw = new JsonTextWriter(sw)
+            {
+                Formatting = Formatting.None
+            };
 
             JsonSerializer.Create(this._settings).Serialize(jw, input);
 
@@ -46,7 +51,6 @@
             return ms; // caller disposes this stream
         }
 
-
         public class CosmosMetadataPreservingResolver : DefaultContractResolver
         {
             protected override JsonContract CreateContract(Type objectType)
@@ -55,6 +59,7 @@
 
                 // If this type has an ETag property
                 if (typeof(IHaveAnETag).IsAssignableFrom(objectType))
+                {
                     if (contract is JsonObjectContract objectContract)
                     {
                         // Add a mapping: JSON "_etag" ↔ C# "Etag" property
@@ -69,21 +74,50 @@
 
                         objectContract.Properties.Add(etagProperty);
                     }
+                }
 
                 return contract;
             }
 
             public class ETagValueProvider : IValueProvider
             {
-                public void SetValue(object target, object value)
-                {
-                    if (target is IHaveAnETag etagged) etagged.Etag = value?.ToString();
-                }
-
                 public object GetValue(object target)
                 {
                     return (target as IHaveAnETag)?.Etag;
                 }
+
+                public void SetValue(object target, object value)
+                {
+                    if (target is IHaveAnETag etagged) etagged.Etag = value?.ToString();
+                }
+            }
+        }
+
+        public class TypeNameOnlyBinder : ISerializationBinder
+        {
+            public void BindToName(Type serializedType, out string assemblyName, out string typeName)
+            {
+                assemblyName = null;
+                typeName = serializedType.Name; // Just the class name
+            }
+
+            public Type BindToType(string assemblyName, string typeName)
+            {
+                var matches = AppDomain.CurrentDomain.GetAssemblies()
+                                       .SelectMany(a => a.GetTypes())
+                                       .Where(t => t.Name == typeName)
+                                       .ToList();
+    
+                if (matches.Count == 1) return matches[0];
+    
+                if (matches.Count == 0)
+                    throw new JsonSerializationException($"No type found with name '{typeName}'");
+    
+                if (matches.Count > 1)
+                    throw new JsonSerializationException(
+                        $"Multiple types found with name '{typeName}': {string.Join(", ", matches.Select(t => t.FullName))}");
+            
+                return matches[0]; // Won't reach here, but compiler needs it
             }
         }
     }
